84e8a2bfb64cba16e2c76560a94e791dcc1c138d
Doniyor <36728197+Doniyor4120@users.noreply.github.com>
2022-09-08 16:07:02 +0500

    ngrx-data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PostsListComponent } from './posts/posts-list/posts-list.component';
-import { SinglePostComponent } from './posts/single-post/single-post.component';
-import { EditPostComponent } from './posts/edit-post/edit-post.component';
-import { AddPostComponent } from './posts/add-post/add-post.component';
-import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PostsListComponent} from './posts/posts-list/posts-list.component';
+import {SinglePostComponent} from './posts/single-post/single-post.component';
+import {EditPostComponent} from './posts/edit-post/edit-post.component';
+import {AddPostComponent} from './posts/add-post/add-post.component';
+import {HomeComponent} from './home/home.component';
+import { EntityDataModule } from '@ngrx/data';
+import { entityConfig } from './entity-metadata';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import {HttpClientModule} from "@angular/common/http";
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,9 +27,15 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    EntityDataModule.forRoot(entityConfig),
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/entity-metadata.ts
@@ -0,0 +1,9 @@
+import { EntityMetadataMap, EntityDataModuleConfig } from '@ngrx/data';
+
+const entityMetadata: EntityMetadataMap = {
+  Post: {}
+};
+
+export const entityConfig: EntityDataModuleConfig = {
+  entityMetadata,
+};
